e174af9ef5951b6567cb80ecc5a659b3536ccbf0
ohhyogeun <ohhyogeun@khu.ac.kr>
2019-08-31 15:24:11 +0900

    190831 15:24

--- calculator/controller.py
+++ calculator/controller.py
@@ -16,14 +16,14 @@ class CalculatorController:
         tf.reset_default_graph()
         with tf.Session() as sess:
             sess.run(tf.global_variables_initializer())    # 변수 초기화
-            saver = tf.train.import_meta_graph('calculator/saved_add_model/model-1000.meta')
-            saver.restore(sess, tf.train.latest_checkpoint('calculator/saved_add_model'))    # 가장 최근화 된 것
+            saver = tf.train.import_meta_graph('calculator/saved_'+opcode+'_model/model-1000.meta')
+            saver.restore(sess, tf.train.latest_checkpoint('calculator/saved_'+opcode+'_model'))    # 가장 최근화 된 것
 
             graph = tf.get_default_graph()    # 그래프 처리
             w1 = graph.get_tensor_by_name('w1:0')
             w2 = graph.get_tensor_by_name('w2:0')
             feed_dict = {w1:float(n1), w2:float(n2)}
-            op_to_restore = graph.get_tensor_by_name('op_add:0')
+            op_to_restore = graph.get_tensor_by_name('op_'+opcode+':0')
             result = sess.run(op_to_restore, feed_dict)
             print('텐서가 계산한 결과 : {}'.format(result))
         return result
